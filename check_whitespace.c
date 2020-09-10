05f5982e536509dca2af325876664534f3ce42e3
hoomzfwp <ndamte@gmail.com>
2020-09-09 19:56:44 -0500

    Fix the memory leak

--- check_whitespace.c
+++ check_whitespace.c
@@ -46,6 +46,7 @@ char* strip(char* str) {
   // Place the null terminator at the end of the result string.
   result[i-first_non_space] = '\0';
 
+  //foolishly tried to free result here ahaha
   return result;
 }
 
@@ -66,7 +67,10 @@ int is_clean(char* str) {
   // 0 if they're equal, and a positive value if the first is
   // greater than the second.
   result = strcmp(str, cleaned);
-
+  //free(cleaned);
+  if (strlen(cleaned) != 0) {
+	free(cleaned);
+  }
   return result == 0;
 }
 
